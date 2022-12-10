3d1c6e6f954127d2620b76cd280957039cbf0467
brandon-schabel <brandonschabel1995@gmail.com>
2022-12-09 21:55:49 -0700

    remove files for starter

--- app/routes/api.get-block.$id.tsx
+++
@@ -1,8 +0,0 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { retrieveNotionBlock } from "~/utils/notion.server";
-
-export async function loader({ request, params }: LoaderArgs) {
-  const blockData = await retrieveNotionBlock(params.id || "");
-
-  return { blockData };
-}
