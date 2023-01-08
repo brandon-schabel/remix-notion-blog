1d6294910c64a02aa530f3fa174a1e454697e82c
brandon-schabel <brandonschabel1995@gmail.com>
2023-01-08 13:40:25 -0700

    run inject on mount

--- app/root.tsx
+++ app/root.tsx
@@ -10,11 +10,10 @@ import {
 } from "@remix-run/react";
 import { inject } from "@vercel/analytics";
 import { Analytics } from "@vercel/analytics/react";
+import { useEffect } from "react";
 import Navbar from "./components/navbar";
 
 import styles from "./styles/app.css";
-
-inject();
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -38,6 +37,9 @@ declare global {
 
 export default function App() {
   const { ENV } = useLoaderData<typeof loader>();
+  useEffect(() => {
+    inject();
+  }, []);
   return (
     <html lang="en">
       <head>
